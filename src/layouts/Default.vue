15363455e85247e624c553f76a84977e0959e552
DanielHajduk98 <danhajduk@protonmail.com>
2021-07-08 16:22:19 +0200

    :tada: init

---
+++ src/layouts/Default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout">
+    <header class="header"></header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+html,
+body {
+  height: 100%;
+  overflow-x: hidden;
+}
+
+body {
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+</style>
