ecd80f3734e157bc9bb8a47aed46ce616cecc019
DanielHajduk98 <danhajduk@protonmail.com>
2021-07-11 13:42:21 +0200

    :sparkles: Added piano note press

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,9 +5,21 @@
         <transition name="resize" v-on:after-enter="afterEnter">
           <div v-if="reveal" class="reveal"></div>
         </transition>
+
         <div v-if="showPiano" class="piano-wrapper">
-          <div class="piano">piano</div>
+          <div class="piano">
+            <button
+              :ref="`btn${index === 10 ? 0 : index}`"
+              @click="play(index === 10 ? 0 : index)"
+              class="piano__note"
+              v-for="index in 10"
+              :key="index"
+            >
+              <span>{{ `btn${index === 10 ? 0 : index}` }}</span>
+            </button>
+          </div>
         </div>
+
         <div v-else class="text">
           <h1 class="text__title">🎉</h1>
           <h4 class="text__subtitle">Made to waste your time!</h4>
@@ -31,6 +43,20 @@ export default {
   },
 
   methods: {
+    pressNote(note) {
+      const btnRef = this.$refs[`btn${note}`][0];
+
+      btnRef.classList.add("active");
+      btnRef.click();
+      setTimeout(() => {
+        btnRef.classList.remove("active");
+      }, 100);
+    },
+
+    play(note) {
+      console.log(note);
+    },
+
     swapElements() {
       setTimeout(() => {
         this.reveal = true;
@@ -45,6 +71,15 @@ export default {
 
   mounted() {
     this.swapElements();
+
+    let self = this;
+    window.onkeyup = function (event) {
+      const key = event.key;
+
+      if (self.showPiano && !isNaN(key) && key !== " ") {
+        self.pressNote(key);
+      }
+    };
   },
 };
 </script>
@@ -114,5 +149,34 @@ export default {
 
 .piano {
   text-align: center;
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-gap: 2px;
+  width: 30%;
+  max-width: 500px;
+
+  &__note {
+    background: #fff;
+    height: 125px;
+    border-radius: 0 0 15px 15px;
+    box-shadow: 2px 2px 4px gray;
+    border: none;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+
+    & > span {
+      font-size: 1rem;
+      text-align: center;
+      margin-bottom: 10px;
+    }
+
+    &.active,
+    &:active {
+      box-shadow: 2px 2px 4px gray, 1px 0px rgba(0, 0, 0, 0.1) inset,
+        -1px 0px rgba(0, 0, 0, 0.1) inset;
+      background: linear-gradient(to bottom, #fff 0%, #eee 100%);
+    }
+  }
 }
 </style>
