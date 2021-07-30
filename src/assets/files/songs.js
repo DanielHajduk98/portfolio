51074b58a090a3ca79be3fbee2e7973054d123d4
DanielHajduk98 <danhajduk@protonmail.com>
2021-07-30 17:12:59 +0200

    :recycle: Moved songs to separate file

---
+++ src/assets/files/songs.js
@@ -0,0 +1,27 @@
+import Song from "@/assets/objects/song";
+
+export default [
+  new Song("Imperial march", "3 3 33 1 53 1 53 7 7 77 8 53 1 53"),
+  new Song(
+    "Godfather",
+    "3    6 8 7 6   8 6 7 6   4 5 3     3   6 8 7 6    8 6 7 6    3 3 2     2 4 6 7     1 3 4 6 "
+  ),
+  new Song("Mc Donald's theme", "123 65"),
+  new Song(
+    "Astronomia aka coffin dance",
+    "2 2 6 5 4 3  3 4 5  4 3 2  2 4 3 4 3 4   2  2 4 3 4 3 4"
+  ),
+  new Song(
+    "Happy birthday",
+    "1 1 2 1 4 3, 1 1 2 1 5 4, 1 1 8 6 4 3 2, 5 5 2 3 4"
+  ),
+  new Song(
+    "Tetris",
+    "0 789 876 809 877 890 8 6 6 2 46 543 809, 8, 7, 7, 8, 9, 0, 8, 6, 6"
+  ),
+  new Song("Force theme", "36 7 8-9-8 3 3-6 7 8 3 8 6 0 9"),
+  new Song(
+    "Государственный гимн СССР",
+    "8      5 8    5 6 7   3 3 6   5 4 5   1 1   2    2 3 4    4 5 6    7 8 9    5 5 0    9 8 9    5 5 8    7 6 7    3 3 6    5 4 5    1 1 8    7 6 5 0   9   8 7 8 9   5 5   8   7 6 5 6 7    3 3    8   6 7 8    6 7 8   6 8 4    4   0 9 8 9 0   8 8   9   8 7 6 7 8   6 6    8   7   6 5   1   1 5     6   7   8!"
+  ),
+];
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -48,6 +48,7 @@ import B4 from "@/assets/sounds/piano/b5.mp3";
 import C5 from "@/assets/sounds/piano/c5.mp3";
 import D5 from "@/assets/sounds/piano/d5.mp3";
 import Song from "../assets/objects/song";
+import songs from "@/assets/files/songs.js";
 
 export default {
   data() {
@@ -78,29 +79,9 @@ export default {
   methods: {
     *notesGenerator() {
       while (true) {
-        yield new Song("Imperial march", "3 3 33 1 53 1 53 7 7 77 8 53 1 53");
-        yield new Song(
-          "Godfather",
-          "3    6 8 7 6   8 6 7 6   4 5 3     3   6 8 7 6    8 6 7 6    3 3 2     2 4 6 7     1 3 4 6 "
-        );
-        yield new Song("Mc Donald's theme", "123 65");
-        yield new Song(
-          "Astronomia aka coffin dance",
-          "2 2 6 5 4 3  3 4 5  4 3 2  2 4 3 4 3 4   2  2 4 3 4 3 4"
-        );
-        yield new Song(
-          "Happy birthday",
-          "1 1 2 1 4 3, 1 1 2 1 5 4, 1 1 8 6 4 3 2, 5 5 2 3 4"
-        );
-        yield new Song(
-          "Tetris",
-          "0, 7, 8, 9, 8, 7, 6, 8, 0, 9, 8, 7, 7, 8, 9, 0, 8, 6, 6, 2, 4, 6, 5, 4, 3, 8, 0, 9, 8, 7, 7, 8, 9, 0, 8, 6, 6"
-        );
-        yield new Song("Force theme", "36 7 8-9-8 3 3-6 7 8 3 8 6 0 9");
-        yield new Song(
-          "Государственный гимн СССР",
-          "8      5 8    5 6 7   3 3 6   5 4 5   1 1   2    2 3 4    4 5 6    7 8 9    5 5 0    9 8 9    5 5 8    7 6 7    3 3 6    5 4 5    1 1 8    7 6 5 0   9   8 7 8 9   5 5   8   7 6 5 6 7    3 3    8   6 7 8    6 7 8   6 8 4    4   0 9 8 9 0   8 8   9   8 7 6 7 8   6 6    8   7   6 5   1   1 5     6   7   8!"
-        );
+        for (let song of songs) {
+          yield new Song(song.title, song.notes);
+        }
       }
     },
 
@@ -136,7 +117,6 @@ export default {
 
   mounted() {
     this.swapElements();
-
     this.nextSong();
 
     let self = this;
